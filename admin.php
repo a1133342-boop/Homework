fb00983794d1945b28f5107b339e0aebaaea57c0
a1133342-boop <a1133342@mail.nuk.edu.tw>
2026-04-17 02:17:03 +0800

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['role']) || $_SESSION['role'] != "admin"){
+    echo "無權限！";
+    exit();
+}
+?>
+
+<h1>管理者頁面</h1>
+
+<?php
+echo "歡迎：" . $_SESSION['user'] . "<br>";
+echo "Cookie使用者：" . $_COOKIE['user'] . "<br>";
+?>
+
+<a href="logout.php">登出</a><br>
+<a href="clear_cookie.php">刪除Cookie</a>
