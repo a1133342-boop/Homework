d31cf667e1545bd61fb5e002feb1eee32554c0b5
a1133342-boop <a1133342@mail.nuk.edu.tw>
2026-04-17 03:15:39 +0800

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,31 +1,43 @@
 <?php
 session_start();
 
-if(!isset($_SESSION['pname'])){
-    echo "目前沒有商品<br>";
+if(!isset($_SESSION['cart'])){
+    echo "購物車是空的<br>";
     echo "<a href='menu.php'>回商品頁</a>";
     exit();
 }
 
-$name = $_SESSION['pname'];
-$price = $_SESSION['price'];
-$amount = $_SESSION['amount'];
-$brand = $_SESSION['brand'];
+$total_all = 0;
 
-$total = $price * $amount;
+echo "<h2>購物車內容</h2>";
+
+foreach($_SESSION['cart'] as $item){
+
+    $name = $item['name'];
+    $price = $item['price'];
+    $amount = $item['amount'];
+    $brand = $item['brand'];
+
+    $total = $price * $amount;
+    $total_all += $total;
+
+    echo "商品：$name<br>";
+    echo "品牌：$brand<br>";
+    echo "單價：$price<br>";
+    echo "數量：$amount<br>";
+    echo "小計：$total<br>";
+    echo "------------------<br>";
+}
+
+echo "總金額：$total_all<br>";
+
+
+if(isset($_COOKIE['last_product'])){
+    echo "<br>最近商品（Cookie）：".$_COOKIE['last_product']."<br>";
+}
+
+
+echo "<br><a href='delete.php'>清空購物車</a>";
+echo "<br><a href='menu.php'>繼續購買</a>";
 ?>
 
-<h2>購物車內容</h2>
-
-商品名稱：<?php echo $name; ?><br>
-品牌：<?php echo $brand; ?><br>
-單價：<?php echo $price; ?><br>
-數量：<?php echo $amount; ?><br>
-總金額：<?php echo $total; ?><br>
-
-<br>
-最近瀏覽（Cookie）：<?php echo $_COOKIE['last_product']; ?><br>
-
-<br>
-<a href="delete.php">清空購物車</a><br>
-<a href="menu.php">繼續購買</a>
